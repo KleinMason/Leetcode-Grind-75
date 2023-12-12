a9dfd5437fc25b4588561ffef120e74323c77815
Mason Kleinsorge <Kleinsorgem97@outlook.com>
2023-12-12 11:04:24 -0600

    Adding _ProductOfArrayExceptSelt.cs

---
+++ cs/LeetCode/LeetCode/Array/_ProductOfArrayExceptSelt.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Array
+{
+    internal class _ProductOfArrayExceptSelt
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int[] results = new int[nums.Length];
+            int multiplier = 1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                results[i] = multiplier;
+                multiplier *= nums[i];
+            }
+
+            multiplier = 1;
+
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                results[i] *= multiplier;
+                multiplier *= nums[i];
+            }
+
+            return results;
+        }
+    }
+}
