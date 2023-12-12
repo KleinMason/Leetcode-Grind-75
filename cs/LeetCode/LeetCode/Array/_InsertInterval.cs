8188e2f5ab326afcabbef21eb8864f0191d650c7
Mason Kleinsorge <Kleinsorgem97@outlook.com>
2023-12-11 18:33:40 -0600

    Add InsertInterval

---
+++ cs/LeetCode/LeetCode/Array/_InsertInterval.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Array
+{
+    internal class _InsertInterval
+    {
+        public int[][] Insert(int[][] intervals, int[] newInterval)
+        {
+            var result = new List<int[]>();
+
+            foreach (var interval in intervals)
+            {
+                if (interval[0] > newInterval[1])
+                {
+                    result.Add(newInterval);
+                    newInterval = interval;
+                }
+                else if (interval[1] < newInterval[0])
+                {
+                    result.Add(interval);
+                }
+                else
+                {
+                    newInterval[0] = Math.Min(interval[0], newInterval[0]);
+                    newInterval[1] = Math.Max(interval[1], newInterval[1]);
+                }
+            }
+
+            result.Add(newInterval);
+            return result.ToArray();
+        }
+    }
+}
